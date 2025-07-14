44d791d4de21d6f0e5dedb01df785a66746d7103
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2025-07-14 15:56:57 -0300

    :construction: criada a classe DTO
    :construction: criada a classe DTO

---
+++ Mini_Project/src/main/java/com/example/Mini_Project/dto/UsuarioDTO.java
@@ -0,0 +1,5 @@
+    package com.example.Mini_Project.dto;
+
+    public record UsuarioDTO(String nome, String email) {
+        
+    }
--- Mini_Project/src/main/java/com/example/Mini_Project/model/Usuario.java
+++ Mini_Project/src/main/java/com/example/Mini_Project/model/Usuario.java
@@ -20,6 +20,35 @@ public class Usuario {
 
     
     
+    public Usuario() {
+    }
+    
+    public Usuario(Integer id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+    
 
 
     
