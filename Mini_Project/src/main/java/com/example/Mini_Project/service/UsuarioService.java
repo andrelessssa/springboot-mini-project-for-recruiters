2cbbf98390db17195e9871120640b72eb1755ba9
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2025-07-15 20:53:43 -0300

    :construction: construida a camada Service
    :construction: construída a camada Service

--- Mini_Project/src/main/java/com/example/Mini_Project/service/UsuarioService.java
+++ Mini_Project/src/main/java/com/example/Mini_Project/service/UsuarioService.java
@@ -1,7 +1,13 @@
 package com.example.Mini_Project.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import com.example.Mini_Project.dto.UsuarioDTO;
+import com.example.Mini_Project.model.Usuario;
 import com.example.Mini_Project.repository.UsuarioRepository;
 
 @Service
@@ -10,8 +16,32 @@ public class UsuarioService {
     private UsuarioRepository usuarioRepository;
         
     
-        public UsuarioService(UsuarioRepository repository) {
-            this.usuarioRepository = repository;
+    public UsuarioService(UsuarioRepository repository) {
+        this.usuarioRepository = repository;
     }
+    public Usuario criar(UsuarioDTO usuarioDTO){
+        Usuario usuario = new Usuario();
+        BeanUtils.copyProperties(usuarioDTO, usuario);
+        return usuarioRepository.save(usuario);
+    }
+    public List<Usuario> listar(){
+        return usuarioRepository.findAll();
+    }
+    public Usuario buscarPorNome(String nome){
+        Optional<Usuario> usuario = usuarioRepository.findByNome(nome);
+        return usuario.orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    }
+    public Usuario atualizar(Integer id, UsuarioDTO usuarioDTO){
+        Usuario usuario = usuarioRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Usuário com ID " + id + " não encontrado."));
+        BeanUtils.copyProperties(usuarioDTO, usuario, "id");
+        return usuarioRepository.save(usuario);
+    }
+    public void deletar(Integer id){
+        usuarioRepository.deleteById(id);
+
+    }
+
+
 
 }
